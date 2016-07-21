33238c7190e2ff551e1c7ad9cc988548b51aeba6
erwinkarim <erwinkarim@gmail.com>
2016-07-21 16:25:43 +0800

    moved code in app.js into ready.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,36 +15,14 @@
 //= require jquery.timepicker.js
 //= require jquery_ujs
 //= require turbolinks
+//= require snackbar
 //= require remarkable.min.js
 //= require featherlight
 //= require featherlight-gallery
-//= require snackbar
 //= require jquery-fileupload/basic-plus
 //= require utilities
 //= require react
 //= require react_ujs
 //= require components
+//= require ready
 //= require_tree .
-
-var ready = function(){
-  //convert flash messages into snackbar
-  $.each( flashMessages, function(key, value){
-    $.snackbar({content: value, style: key, timeout: 10000});
-  });
-
-  //featherlight gallery caption
-  $.featherlightGallery.prototype.afterContent = function() {
-    console.log("$currentTarget", this.$currentTarget[0].dataset.caption);
-    var caption = this.$currentTarget[0].dataset.caption;
-    this.$legend = this.$legend || $('<div class="legend"/>').insertAfter(this.$content);
-    this.$legend.text(caption);
-    /*
-    this.$instance.find('.caption').remove();
-    $('<div class="caption">').text(caption).appendTo(this.$instance.find('.featherlight-content'));
-    */
-  };
-
-};
-
-$(document).ready(ready);
-$(document).on('page:load', ready);
