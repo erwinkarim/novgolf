5b55b22e1b9b435b45f33ad0cce16cd0ea369668
erwinkarim <erwinkarim@gmail.com>
2016-07-26 22:30:27 +0800

    fixtures for golf_club, flight_schedules, flight_matrices, charge_schedule and some users; basic function checks for golf_club

--- test/models/golf_club_test.rb
+++ test/models/golf_club_test.rb
@@ -6,6 +6,14 @@ class GolfClubTest < ActiveSupport::TestCase
   # end
 
   test "search exists" do
-    assert true
+    assert_respond_to GolfClub, :search
+  end
+
+  test "setFlightSchedule exists in instance" do
+      assert_respond_to GolfClub.first, :setFlightSchedule
+  end
+
+  test "amenity_listings exists instance" do
+    assert_respond_to GolfClub.first, :amenity_listings
   end
 end
