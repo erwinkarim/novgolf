abf19a906f7f9f568e1a5834c41b60e40c76fefc
erwinkarim <erwinkarim@gmail.com>
2016-10-14 18:39:55 +0800

    rewrite dashboard

--- app/assets/javascripts/components/golf_club_dashboard.js.jsx
+++ app/assets/javascripts/components/golf_club_dashboard.js.jsx
@@ -1,275 +1,49 @@
-var OverlayTrigger = ReactBootstrap.OverlayTrigger;
-var Popover = ReactBootstrap.Popover;
-
-var monthNames = ["January","February","March","April","May","June",
-		"July","August","September","October","November","December"];
-
-function addDays(date, days) {
-	var result = new Date(date);
-	result.setDate(result.getDate() + days);
-	return result;
-}
-
-function getSunday(day) {
-	var date = new Date(day);
-	switch(day.getDay()) {
-		case 0: return date;
-		case 1: return addDays(date, 6);
-		case 2: return addDays(date, 5);
-		case 3: return addDays(date, 4);
-		case 4: return addDays(date, 3);
-		case 5: return addDays(date, 2);
-		case 6: return addDays(date, 1);
-		default: break;
-	}
-}
-
-function getMonday(day) {
-	var date = new Date(day);
-	switch(day.getDay()) {
-		case 0: return addDays(date, -6);
-		case 1: return date;
-		case 2: return addDays(date, -1);
-		case 3: return addDays(date, -2);
-		case 4: return addDays(date, -3);
-		case 5: return addDays(date, -4);
-		case 6: return addDays(date, -5);
-		default: break;
-	}
-}
-
-function getTuesday(day) {
-	var date = new Date(day);
-	switch(day.getDay()) {
-		case 0: return addDays(date, -5);
-		case 1: return addDays(date, 1);
-		case 2: return date;
-		case 3: return addDays(date, -1);
-		case 4: return addDays(date, -2);
-		case 5: return addDays(date, -3);
-		case 6: return addDays(date, -4);
-		default: break;
-	}
-}
-
-function getWednesday(day) {
-	var date = new Date(day);
-	switch(day.getDay()) {
-		case 0: return addDays(date, -4);
-		case 1: return addDays(date, 2);
-		case 2: return addDays(date, 1);
-		case 3: return date;
-		case 4: return addDays(date, -1);
-		case 5: return addDays(date, -2);
-		case 6: return addDays(date, -3);
-		default: break;
-	}
-}
-
-function getThursday(day) {
-	var date = new Date(day);
-	switch(day.getDay()) {
-		case 0: return addDays(date, -3);
-		case 1: return addDays(date, 3);
-		case 2: return addDays(date, 2);
-		case 3: return addDays(date, 1);
-		case 4: return date;
-		case 5: return addDays(date, -1);
-		case 6: return addDays(date, -2);
-		default: break;
-	}
-}
-
-function getFriday(day) {
-	var date = new Date(day);
-	switch(day.getDay()) {
-		case 0: return addDays(date, -2);
-		case 1: return addDays(date, 4);
-		case 2: return addDays(date, 3);
-		case 3: return addDays(date, 2);
-		case 4: return addDays(date, 1);
-		case 5: return date;
-		case 6: return addDays(date, -1);
-		default: break;
-	}
-}
-
-function getSaturday(day) {
-	var date = new Date(day);
-	switch(day.getDay()) {
-		case 0: return addDays(date, -1);
-		case 1: return addDays(date, 5);
-		case 2: return addDays(date, 4);
-		case 3: return addDays(date, 3);
-		case 4: return addDays(date, 2);
-		case 5: return addDays(date, 1);
-		case 6: return date;
-		default: break;
-	}
-}
 
 var GolfClubDashboard = React.createClass({
   propTypes: {
-    club: React.PropTypes.object,
-    paths: React.PropTypes.object,
-    data: React.PropTypes.object,
-    queryDate: React.PropTypes.string,
-    selectedDays: React.PropTypes.array
-  },
-  getDefaultProps: function(){
+      club:React.PropTypes.object,
+      paths:React.PropTypes.object
   },
   getInitialState: function(){
-	var monDate = new Date(getMonday(new Date()));
-	var tuesDate = new Date(getTuesday(new Date()));
-	var wednesDate = new Date(getWednesday(new Date()));
-	var thursDate = new Date(getThursday(new Date()));
-	var friDate = new Date(getFriday(new Date()));
-	var saturDate = new Date(getSaturday(new Date()));
-	var sunDate = new Date(getSunday(new Date()));
+    var today = new Date();
+    days = arrayFromRange(0,6).map( (e,i) => {
+        newDay = new Date();
+        newDay.setDate(today.getDate() + e);
+        return newDay.getDate() + '/' + (newDay.getMonth() + 1) + '/' + newDay.getFullYear();
+    });
 
     return {
-    	selectedDays:[{"day":"Monday","date":monDate},{"day":"Tuesday","date":tuesDate},{"day":"Wednesday","date":wednesDate},{"day":"Thursday","date":thursDate},{"day":"Friday","date":friDate},{"day":"Saturday","date":saturDate},{"day":"Sunday","date":sunDate}],
-    	data:null, queryDate:'' // $.datepicker.formatDate('dd/mm/yy', new Date())
+      flightsArray:[],
+      days: days
     }
   },
-  componentDidMount: function(){
-	var handle = this;
-	$(this.refs.queryDate).datepicker({
-        onSelect: function(dateText){
-            handle.dateChanged(dateText);
-            return;
-        },
-        dateFormat:'dd/mm/yy'
-      });
-  },
-  dateChanged: function(e){
-    //handle when the date changed
-    //get updated teeTimes
+  componentDidMount:function(){
+    $(this.refs.datepicker).datepicker();
+
+    //load the schedule, today and the next 6 days
     var handle = this;
+    this.state.days.map( (e,i) => {
+      $.getJSON(handle.props.paths.club_path, {date:e}, function(data){
+        var newFlightsArray = handle.state.flightsArray;
+        newFlightsArray[i] = data.flights;
+        handle.setState({flightsArray:newFlightsArray});
+      });
+    });
 
-  	$.getJSON(this.props.paths.club_path, e, function(clubData){
-  		handle.setState({data:clubData})
-  	});
-
-  	handle.setState({queryDate:e})
-  	var datePart = e.split("/");
-  	var day = datePart[2] + "-" + datePart[1] + "-" + datePart[0];
-  	console.log(day);
-  	var date = new Date(day);
-  	handle.setState({selectedDays:[{"day":"Monday","date":new Date(getMonday(date))},
-  		{"day":"Tuesday","date":new Date(getTuesday(date))},
-  		{"day":"Wednesday","date":new Date(getWednesday(date))},
-  		{"day":"Thursday","date":new Date(getThursday(date))},
-  		{"day":"Friday","date":new Date(getFriday(date))},
-  		{"day":"Saturday","date":new Date(getSaturday(date))},
-  		{"day":"Sunday","date":new Date(getSunday(date))}
-  	]});
   },
-  render: function() {
-  	if(this.state.data != null) {
-	    return (
-	      <div>
-      		<input type="text" className="datepicker form-control" ref="queryDate" name="dateQuery" value={this.state.queryDate} style={ {zIndex:1000, position:'relative'} } onChange={this.dateChanged }/>
-      		<br/>
-      		<table className="table table-striped table-bordered table-responsive">
-      			<thead>
-      				<tr>
-      					<th></th>
-      					{this.state.data.flights.map((flight, i) => <TableColumnHeadDashboard key = {i} data = {flight} />)}
-      				</tr>
-        			</thead>
-        			<tbody>
-        				{this.state.selectedDays.map((day, i) => <TableRowDashboard key = {i} data = {this.state.data} selectedDay =  {day} />)}
-        			</tbody>
-        		</table>
-  	      </div>
-	    );
-  	}
-  	else {
-      return (
-        <div>
-          <input type="text" className="datepicker form-control" ref="queryDate" name="dateQuery"
-            value={this.state.queryDate} style={ {zIndex:1000, position:'relative'} } onChange={this.dateChanged }/>
-      		<br/>
-        </div>
-      );
-    }
-  }
-});
-
-var TableColumnHeadDashboard = React.createClass({
-  render: function() {
-  	var myStyle = {
-  		textAlign:'center'
-  	}
-
+  render: function(){
     return (
-    	<th style={myStyle}>
-    		{this.props.data.tee_time}
-    	</th>
-    );
-  }
-});
-
-var TableRowDashboard = React.createClass({
-  render: function() {
-  	var date = new Date(this.props.selectedDay.date);
-    return (
-    	<tr>
-    		<TableRowHeadDashboard data = {this.props.selectedDay.day + " - " + date.getDate() + " " + monthNames[date.getMonth()] + " " + date.getFullYear()} />
-    		{this.props.data.flights.map((flight, i) => <TableRowColumnDashboard key = {i} data = {flight} />)}
-    	</tr>
-    );
-  }
-});
-
-var TableRowHeadDashboard = React.createClass({
-  render: function() {
-  	var myStyle = {
-  		textAlign:'center'
-  	}
-    return (
-    	<th style={myStyle}>
-    		{this.props.data}
-    	</th>
-    );
-  }
-});
-
-var TableRowColumnDashboard = React.createClass({
-  render: function() {
-	var myStyle = {
-		background:'#FFFFFF',
-		padding:0
-	}
-
-	if(this.props.data.reserve_status != null) {
-		if(this.props.data.reserve_status == 0 || this.props.data.reserve_status == 1) {
-			myStyle = {
-				background:'#FFFF00',
-				padding:0
-			}
-		}
-		else {
-			myStyle = {
-				background:'#FF0000',
-				padding:0
-			}
-		}
-		return (
-			<OverlayTrigger trigger="click" rootClose placement="bottom" overlay={<Popover id="test" title="Popover bottom"><strong>Holy Guacamole!</strong>Testing</Popover>}>
-				<td style={myStyle} role="button">
-					Unavailable
-				</td>
-			</OverlayTrigger>
-		    );
-	}
-    return (
-	<OverlayTrigger trigger="click" rootClose placement="bottom" overlay={<Popover id="test" title="Popover bottom"><strong>Available for booking!</strong>Available !!! <a>Book Now</a></Popover>}>
-		<td style={myStyle} role="button">
-			Available
-		</td>
-	</OverlayTrigger>
-    );
+      <div>
+        <input ref="datepicker" type="text" />
+        { this.state.flightsArray.map( (e,i) => {
+          return (
+            <div>
+              <strong>{this.state.days[i]}</strong>
+              <GolfCardTimesGroup flights={e} />
+            </div>
+          );
+        })}
+      </div>
+    )
   }
 });
