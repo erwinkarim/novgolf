cbc90cd08082b1d367de54a0fa489a717c641ecc
erwinkarim <erwinkarim@gmail.com>
2016-11-25 11:16:36 +0800

    new component UserMembershipCard; added to user_profile

---
+++ app/assets/javascripts/components/user_membership_card.js.jsx
@@ -0,0 +1,16 @@
+var UserMembershipCard = React.createClass({
+
+  render: function() {
+    return (
+      <div className="card">
+        <div className="card-header">Membership</div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">None yet...</li>
+        </ul>
+        <div className="card-footer">
+          <a href="#">Edit Membership</a>
+        </div>
+      </div>
+    );
+  }
+});
