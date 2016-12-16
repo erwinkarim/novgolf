98b16a07335e2b7799abe606360b5de851f30f7d
erwinkarim <erwinkarim@gmail.com>
2016-12-16 10:21:20 +0800

    ur_transaction model to track user_reservation changes

---
+++ app/models/ur_transaction.rb
@@ -0,0 +1,2 @@
+class UrTransaction < ActiveRecord::Base
+end
---
+++ test/models/ur_transaction_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UrTransactionTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
