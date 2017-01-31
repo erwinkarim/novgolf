3cec68b2aac3df8981fc8681a0c9e827a4d9a8b3
erwinkarim <erwinkarim@gmail.com>
2017-01-31 13:53:51 +0800

    moved request review mail to mjml

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,7 +12,7 @@ class UserMailer < ApplicationMailer
   # user_reservations is an array of user_reservations
   def reservation_confirmed user_reservations = []
     @reservations = user_reservations
-    mail(to: "erwinkarim@gmail.com", subject: "Your Reservation(s) is Confirmed") do |format|
+    mail(to: @reservations.first.user.email, subject: "Your Reservation(s) is Confirmed") do |format|
       format.text
       format.mjml
     end
--- app/mailers/user_reservation_mailer.rb
+++ app/mailers/user_reservation_mailer.rb
@@ -1,12 +1,15 @@
 class UserReservationMailer < ApplicationMailer
   layout false
-  
+
   def request_review user_reservation
     #if the reservation is still valid, send out mail
     if user_reservation.payment_confirmed? || user_reservation.reservation_confirmed? then
       @reservation = user_reservation
-      @user = User.find(user_reservation.user)
-      mail(to:@user.email, subject:"Review your flight")
+      @user = user_reservation.user
+      mail(to:@user.email, subject:"Review your flight") do |format|
+        format.text
+        format.mjml
+      end
     end
   end
 end
