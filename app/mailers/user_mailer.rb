95b0ffeeb8f0eea2b4696e91d2ac7e26c1255681
erwinkarim <erwinkarim@gmail.com>
2017-01-31 11:53:31 +0800

    change from html to mjml * update reservation_confirmed email to mjml * deleted old text mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,6 +12,9 @@ class UserMailer < ApplicationMailer
   # user_reservations is an array of user_reservations
   def reservation_confirmed user_reservations = []
     @reservations = user_reservations
-    mail(to: "erwinkarim@gmail.com", subject: "Your Reservation(s) is Confirmed")
+    mail(to: "erwinkarim@gmail.com", subject: "Your Reservation(s) is Confirmed") do |format|
+      format.text
+      format.mjml
+    end
   end
 end
