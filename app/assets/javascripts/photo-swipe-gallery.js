62e195c6c7a6788c86c2fde85d8f52d727857aef
erwinkarim <erwinkarim@gmail.com>
2017-03-28 18:24:48 +0800

    rebuild assets for prod

--- app/assets/javascripts/photo-swipe-gallery.js
+++ app/assets/javascripts/photo-swipe-gallery.js
@@ -3,13 +3,8 @@
 
 $(document).ready(function(){
   var initPhotoSwipeFromDOM = function(gallerySelector){
+    /*
     var onThumbnailsClick = function(e){
-      //prevent default stuff
-      e = e || window.event;
-      e.preventDefault ? e.preventDefault() : e.returnValue = false;
-
-      //get the root of the elements
-      var gallery = $(e.target).closest(gallerySelector);
 
       //reconstruct the array of object and open photoswipe on that item
       var object_array = gallery.find('a').map((i,e) => {
@@ -35,8 +30,41 @@ $(document).ready(function(){
       gallery.init();
 
     };
+    */
 
     // loop through all gallery elements and bind events
+    var onThumbnailsClick = function(e){
+      e = e || window.event;
+      e.preventDefault ? e.preventDefault() : e.returnValue = false;
+
+      //get the root of the elements
+      var gallery = $(e.target).closest(gallerySelector);
+
+      var object_array = [];
+
+      $.each(gallery.find('a'), function(index,value){
+          var obj = Object.assign({});
+          var size = value.dataset.size.split('x');
+          obj.w = size[0];
+          obj.h = size[1];
+          obj.src = value.getAttribute('href');
+          obj.title = value.dataset.title;
+
+          object_array.push(obj);
+      });
+
+      var initPS = function(object_array, selected_index){
+        var pswpElement = document.querySelectorAll('.pswp')[0];
+        var options = {index:parseInt(selected_index)};
+
+        var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, object_array, options);
+        gallery.init();
+
+      };
+
+      initPS(object_array,e.target.dataset.index );
+    };
+
     var galleryElements = document.querySelectorAll( gallerySelector );
 
     for(var i = 0, l = galleryElements.length; i < l; i++) {
