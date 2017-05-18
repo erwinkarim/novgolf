e830732f47b320ee1d737a50b77018c5b405c3ec
erwinkarim <erwinkarim@gmail.com>
2017-05-18 18:44:32 +0800

    remove comments

--- db/migrate/20170518092850_add_secondary_flight_to_user_reservation.rb
+++ db/migrate/20170518092850_add_secondary_flight_to_user_reservation.rb
@@ -1,7 +1,6 @@
 class AddSecondaryFlightToUserReservation < ActiveRecord::Migration[5.0]
   def change
     add_column :user_reservations, :second_booking_time, :datetime
-#    add_reference :user_reservations, :second_course_listing, foreign_key: true
     add_reference :user_reservations, :second_course_listing, references: :course_listings, index: true
     add_foreign_key :user_reservations, :course_listings, column: :second_course_listing_id
   end
