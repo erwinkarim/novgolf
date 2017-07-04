c889c4fe9bf9d7b99b7e7247319d83111fd6a1ad
erwinkarim <erwinkarim@gmail.com>
2017-07-04 14:40:19 +0800

    Install mjml during ec2 reboot

--- config/puma.rb
+++ config/puma.rb
@@ -7,6 +7,15 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
+# Specifies the `environment` that Puma will run in.
+begin
+  ENV.fetch("RAILS_ENV")
+rescue IndexError
+  ENV.store("RAILS_ENV", "development")
+end
+#
+environment ENV.fetch("RAILS_ENV")
+
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 #port        ENV.fetch("PORT") { 3000 }
