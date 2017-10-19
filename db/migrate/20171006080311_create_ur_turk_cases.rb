d84057eaaac2fba88694ed178d1313858b47d353
erwinkarim <erwinkarim@Erwins-MacBook-Pro.local>
2017-10-19 13:56:49 +0800

    update foreign_key constraints in AWS

--- db/migrate/20171006080311_create_ur_turk_cases.rb
+++ db/migrate/20171006080311_create_ur_turk_cases.rb
@@ -2,7 +2,8 @@ class CreateUrTurkCases < ActiveRecord::Migration[5.1]
   def change
     create_table :ur_turk_cases do |t|
       t.references :user_reservation, foreign_key: true
-      t.references :user, foreign_key: true
+      # refer in the aws
+      t.references :user, type: :integer, foreign_key: true
       t.timestamps
     end
   end
